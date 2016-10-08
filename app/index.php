9cafabad5d723f58e58d60dca33b059c79621399
Hynnej <hackworj@oregonstate.edu>
2016-10-08 17:37:10 -0600

    Update index.php

--- app/index.php
+++ app/index.php
@@ -2,7 +2,7 @@
 
 	$uri = "mongodb://sirmiq:door5454@ds048319.mlab.com:48319/sports";
 	
-	$client = new MongoClient($uri);
+	//$client = new MongoClient($uri);
 	//$db = $client->selectDB("sports");
 	//$teams = $db->team;
 	//foreach($teams as $doc)
