d04d1072a60ed0e6ccd9886f0a5f73e8edbc7083
Hynnej <hackworj@oregonstate.edu>
2016-10-08 23:57:20 -0600

    Update index.php

--- app/index.php
+++ app/index.php
@@ -23,7 +23,7 @@
 		<div class="row">
 			<div class="col-md-12 text-center">	
 				<h2>Add Team</h2>
-				<form method="post" action="./forms/addTeam.php">
+				<form method="post" action="addTeam.php">
 					<fieldset class="form-group">
 						<p><input type="hidden" name="col" value="team"/></p>
 					<div class="row">
