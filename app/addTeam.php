d48c93ae1e9f2d889e752fae1a68e36e91a9ed35
Hynnej <hackworj@oregonstate.edu>
2016-10-08 23:57:08 -0600

    Create addTeam.php

---
+++ app/addTeam.php
@@ -0,0 +1,28 @@
+<?php
+	//connects to mongodb hosted at mlabs
+	$uri = "mongodb://sirmiq:door5454@ds048319.mlab.com:48319/sports";
+	$client = new MongoClient($uri);
+	$db = $client->selectDB("sports");
+		$teams = $db->team;
+		
+	$lg = $_POST['leagues'];
+	$div = $_POST['divisional'];
+	$nam = $_POST['names'];
+	$webs = $_POST['web'];
+	if($_POST['follows'] == yes)
+		$fol = "Yes";
+	else
+		$fol = "No";
+	
+	$doc = array(
+			"league" : $lg,
+			"division" : $div,
+			"name" : $nam,
+			"website" : $webs,
+			"follows" : "Yes");
+		
+	$teams->insert($doc);
+				/* Redirect browser */
+		header("Location: index.php");	
+	
+?>
