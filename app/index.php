8185a4d1235cb0ceef1e2927230a81ef2f7ab615
Hynnej <hackworj@oregonstate.edu>
2016-10-08 21:15:49 -0600

    Update index.php

--- app/index.php
+++ app/index.php
@@ -1,14 +1,64 @@
-<?php 
+<!--Displays table to allow users to enter data-->
 
+<?php
+	//connects to mongodb hosted at mlabs
 	$uri = "mongodb://sirmiq:door5454@ds048319.mlab.com:48319/sports";
-	//phpinfo();
 	$client = new MongoClient($uri);
 	$db = $client->selectDB("sports");
-	
-	$teams = $db->team;
-	$collection = $teams->find();
-	foreach($collection as $doc)
-	{
-		echo 'Sport ' .$doc['sport'] . "\n";
-	}
+		$teams = $db->team;
 ?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>Sports Tracker</title>
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+</head>
+
+<body>
+<div class="container">
+	<!--nav bar
+	<nav class="navbar navbar-light" style="background-color: #e3f2fd;">
+		<a class="navbar-brand" href="index.html">OSU CS 340 Final Project</a>
+		<ul class="nav navbar-nav">
+			<li class="nav-item">
+				<a class="nav-link" href="food.php">Foods</a>
+				</li>
+			<li class="nav-item">
+			  <a class="nav-link" href="weeklyMeals.php">Weekly Meal Planner</a>
+			</li>
+			<li class="nav-item">
+			  <a class="nav-link" href="list.php">Shopping List</a>
+			</li>			
+		</ul>
+	</nav>-->
+
+	<!--displays table-->
+		<div class="row">
+			<div class="col-md-12 text-center">	
+				<h2 class="text-center">Recipe Database</h2>
+				<table class="table">
+					<thead class="thead-inverse">
+					<tr>
+						<th>League</th>
+						<th>Division</th>
+						<th>Name</th>
+						<th>Website</th>
+						<th>Follow</th>
+					</tr>
+					</thead>
+					<?php
+					//Displays recipe table
+					$collection = $teams->find();
+					foreach($collection as $doc)
+					{
+					echo "<tr><td>" . .$doc['league'] . "</td><td>" . .$doc['division'] . "</td><td>" . .$doc['name'] . "</td><td>" . .$doc['website'] . "</td><td>" . .$doc['follows'] .  "</td></tr>";
+					}
+					?>
+				</table>			
+			</div>
+		</div>
+</div>
+</body>
+</html>
