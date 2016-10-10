e684e1117e8c368cb492837e852b5eb966b70345
Hynnej <hackworj@oregonstate.edu>
2016-10-09 19:39:26 -0600

    Update Teamedit.php

--- app/Teamedit.php
+++ app/Teamedit.php
@@ -5,7 +5,7 @@
 	$db = $client->selectDB("sports");
 	$teams = $db->team;
 	$teamName = $_POST['teamEdit'];
-	$query = array('name' => '$teamName');
+	$query = array('name' => $teamName);
 	$edTeam = $teams->find($query);	
 	var_dump($teamName);
 	var_dump($query);
