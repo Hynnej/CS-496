4e89369538bf21060aa8e43ec0a030aa23c5c87f
Hynnej <hackworj@oregonstate.edu>
2016-10-09 18:15:21 -0600

    Update Teamedit.php

--- app/Teamedit.php
+++ app/Teamedit.php
@@ -5,7 +5,7 @@
 	$db = $client->selectDB("sports");
 	$teams = $db->team;
 	$teamName = $_POST['teamEdit'];
-	$edTeam = $teams->findOne(array('name' => "Seattle Seahawks"));	
+	$edTeam = $teams->findOne(array('name' => $teamName));	
 	echo $edTeam["name"];
 	
 ?>
