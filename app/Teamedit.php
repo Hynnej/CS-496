0238ee37c69c72608195918a9cb73ef522e34d57
Hynnej <hackworj@oregonstate.edu>
2016-10-09 18:35:38 -0600

    Update Teamedit.php

--- app/Teamedit.php
+++ app/Teamedit.php
@@ -5,7 +5,7 @@
 	$db = $client->selectDB("sports");
 	$teams = $db->team;
 	$teamName = $_POST['teamEdit'];
-	$edTeam = $teams->findOne(array('name' => new MongoRegex($teamName)));	
+	$edTeam = $teams->findOne(array('name' => $teamName));	
 	echo  $teamName . ' ' . $edTeam["name"];
 	
 ?>
