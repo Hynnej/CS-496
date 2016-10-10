2b4fd8ef526e9e66b6d332faf48a88bf50562677
Hynnej <hackworj@oregonstate.edu>
2016-10-09 19:54:01 -0600

    Add files via upload

--- app/Teamedit.php
+++ app/Teamedit.php
@@ -1,3 +1,5 @@
+<!--Displays table-->
+
 <?php
 	//connects to mongodb hosted at mlabs
 	$uri = "mongodb://sirmiq:door5454@ds048319.mlab.com:48319/sports";
@@ -10,10 +12,103 @@
         //$teamName = "Seattle Mariners";
 	$query = array('name' => $teamName);
 	$edTeam = $teams->findOne($query);	
-	var_dump($teamName);
-	var_dump($query);
-	var_dump($edTeam);
 	
-		
 ?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>Sports Tracker</title>
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+</head>
+
+<body>
+<div class="container">
+	<!--nav bar-->
+	<nav class="navbar navbar-light" style="background-color: #e3f2fd;">
+		<a class="navbar-brand">Sports Tracker</a>
+		<ul class="nav navbar-nav">
+			<li class="nav-item">
+				<a class="nav-link" href="index.php">Add Team</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="viewData.php">Teams</a>
+			</li>
+			<li>
+			<form method="post" class="form-inline" action="Teamedit.php">
+				<div class="form-group">
+					<select class="c-select" name="teamEdit">
+							<option selected>Edit Team</option>						
+							<?php
+								$collection = $teams->find();
+								foreach($collection as $doc)
+								{	
+									echo '<option value=" '. $doc["name"] . ' "> ' . $doc["name"] . '</option>\n';
+								}
+							$client->close();	
+							?>
+						</select>
+					</div>
+					<input type="submit" class="btn btn-primary" name="editTeam" value="EditTeam"/>
+				</form>
+			</li>
+		</ul>	
+	</nav>
+	
+		<!--Form to add team-->
+	
+		<div class="row">
+			<div class="col-md-12 text-center">	
+				<h2>Edit Team</h2>
+				<form method="post" action="editTeam.php">
+					<fieldset class="form-group">
+						<p><input type="hidden" name="original" value="<?php echo $edTeam["name"];?>"/></p>		
+					<div class="row">
+					<div class="col-md-2 text-center">		
+						<legend>League</legend>
+						<div class="form-check">
+						<label class="form-check-label">
+							<input class="form-check-input" type="radio" name="leagues" value="NFL" <?php if($edTeam["league"] == "NFL") echo 'checked="checked"'; ?>>NFL</label>
+						</div>
+						<div class="form-check">
+						  <label class="form-check-label">
+							<input class="form-check-input" type="radio" name="leagues" value="MLB" <?php if($edTeam["league"] == "MLB") echo 'checked="checked"'; ?>>MLB</label>
+						</div>
+					</div>	
+					<div class="col-md-3 text-center">	
+						<legend>Division</legend>
+						<p><input type="text" name="divisional" value="<?php echo $edTeam["division"];?>"/></p>
+						
+					</div>	
+					<div class="col-md-3 text-center">	
+						<legend>Team Name</legend>
+						<p><input type="text" name="names" value="<?php echo $edTeam["name"];?>"/></p>						
+					</div>
+
+					<div class="col-md-3 text-center">						
+						<legend>Team Website</legend>
+						<p><input type="url" name="web" value="<?php echo $edTeam["website"];?>"/></p>	
+					</div>	
+					<div class="col-md-1 text-center">
+						<legend>Follows</legend>
+						<label class="form-check-inline">
+							</legend> <input class="form-check-input" type="checkbox" name="follows" value="yes" <?php if($edTeam["follows"] == "Yes") echo 'checked="checked"'; ?>> Yes
+						</label>
+					</div>
+					</div>
+
+
+						<p><input type="submit" class="btn btn-success" name="edit" value="edit"/></p>
+
+
+					</fieldset>
+				</form>					
+				</div>
+			</div>
+
+</div>
+</body>
+</html>	
+	
 	
